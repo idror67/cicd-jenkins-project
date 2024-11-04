d4f50510568c7ca986183ec024c612649e1d9ed7
idror <144365018+idror67@users.noreply.github.com>
2024-11-04 22:32:52 +0200

    fix

--- FLASK-CONTACTS-APP/app.py
+++ FLASK-CONTACTS-APP/app.py
@@ -91,5 +91,8 @@ def search():
 
 
 
+#if __name__ == '__main__':
+ #   app.run(debug=True ,port=5052,  host='0.0.0.0')
 if __name__ == '__main__':
-    app.run(debug=True ,port=5052,  host='0.0.0.0')
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=5052)
