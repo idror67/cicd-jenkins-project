5b7818dfd39b463e8ff998a25afaf0488d156d9c
idror <144365018+idror67@users.noreply.github.com>
2024-11-04 23:11:36 +0200

    refactor: reorganize imports and clean up unused routes in app.py

--- FLASK-CONTACTS-APP/app.py
+++ FLASK-CONTACTS-APP/app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, redirect
-from dotenv import load_dotenv
+from flask import Flask, redirect, render_template, request
 import os
-load_dotenv()
+from dotenv import load_dotenv
 
+load_dotenv()
 
 # Retrieve the DATABASE_TYPE environment variable, defaulting to 'MYSQL' if not set
 db_to_use = os.getenv("DATABASE_TYPE", "MYSQL")
@@ -20,57 +20,9 @@ elif db_to_use == "MONGO":
 
 app = Flask(__name__)
 
-
-
-
 @app.route('/')
 def hello():
     return redirect('/viewContacts')
-
-
-@app.route('/addContact', methods=['GET', 'POST'])
-def addContact():
-    return render_template('addContactForm.html')
-
-
-# route to view the contact list
-@app.route('/viewContacts')
-def viewContacts():
-    print(get_contacts())
-    return render_template('index.html', contacts=get_contacts())
-
-
-
-@app.route('/createContact', methods=['POST'])
-def createContact():
-    # adding additional contact to the database (contacts_list)
-    fullname = request.form['fullname']
-    email = request.form['email']
-    phone = request.form['phone']   
-    gender = request.form['gender']
-    photo = request.files['photo']
-    if not check_contact_exist(fullname, email):
-        if photo:
-            # create a name for the file to be saved
-            file_path = 'static/images/' + fullname + '.jpg'
-            # save the file in the server
-            photo.save(file_path)
-        # create a new contact
-        create_contact(fullname, phone, email, gender, f'{fullname}.jpg')
-    else:
-        return render_template('addContactForm.html', message='Contact already exists')
-    return redirect('/viewContacts')
-
-@app.route('/deleteContact/<number>')
-def deleteContact(number):
-    delete_contact(number)
-    return redirect('/viewContacts')
-
-
-@app.route('/editContact/<number>')
-def editContact(number):
-    contact = findByNumber(number)
-    return render_template('editContactForm.html', contact=contact)
 
 @app.route('/saveUpdatedContact/<number>', methods=['POST'])
 def saveUpdatedContact(number):
@@ -81,18 +33,12 @@ def saveUpdatedContact(number):
     update_contact_in_db(number, name, phone, email, gender)
     return redirect('/viewContacts')
 
-
-# search route to filter the contact list according to the search criteria:
 @app.route('/search', methods=['POST'])
 def search():
     search_name = request.form['search_name']
     search_results = search_contacts(search_name)
     return render_template('index.html', contacts=search_results)
 
-
-
-#if __name__ == '__main__':
- #   app.run(debug=True ,port=5052,  host='0.0.0.0')
 if __name__ == '__main__':
     from waitress import serve
     serve(app, host='0.0.0.0', port=5052)
